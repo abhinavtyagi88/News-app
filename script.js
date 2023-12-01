b0bae6dba45db52493f8bd88ccf780a2f9ca3e85
abhinavtyagi88 <132747922+abhinavtyagi88@users.noreply.github.com>
2023-12-01 23:38:19 +0530

    Api_key changed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const API_KEY = "0eb654cb92c74161afbc78b32e131981";
 const url = "https://newsapi.org/v2/everything?q=";
-0eb654cb92c74161afbc78b32e131981
+
 
 window.addEventListener("load", () => fetchNews("India"));
 
